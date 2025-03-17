aa22644c0d7ea132240bd2635a8244b7aea906de
vworn <jasonkmathewpersonal@gmail.com>
2025-03-16 20:23:24 -0400

    Task 1 - Base Structure Setup.

---
+++ cc_15.js
@@ -0,0 +1,8 @@
+// Task 1: Base Structure Setup
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Risk Dashboard Loaded"); // Prints message to console when page loads
+
+    // Selecting elements from the HTML
+    const riskDashboard = document.getElementById("riskDashboard");
+    const riskForm = document.getElementById("riskForm");
+    const increaseRiskButton = document.getElementById("increaseRiskLevels");
